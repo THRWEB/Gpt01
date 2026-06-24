d9a3cfc0eb040913dd435bb5e76389a37ce5c718
thr <t31725@gmail.com>
2026-06-24 22:12:10 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,12 +5,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "..."
+  apiKey: "AIzaSyBzs07mEGjaGXb-aiGY73aS0jBQcavgx7Q",
+  authDomain: "gpt01-fc6ab.firebaseapp.com",
+  projectId: "gpt01-fc6ab",
+  storageBucket: "gpt01-fc6ab.firebasestorage.app",
+  messagingSenderId: "903590395329",
+  appId: "1:903590395329:web:aee13172977e3ca143c340",
+  measurementId: "G-H0PRVKC18F"
 };
 
 const app = initializeApp(firebaseConfig);
