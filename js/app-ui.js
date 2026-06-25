92172736fc0bdc40adaa0255088c5a0b5435c9c1
thr <t31725@gmail.com>
2026-06-25 12:00:59 +0530

    Update app-ui.js

--- js/app-ui.js
+++ js/app-ui.js
@@ -11,88 +11,158 @@ getDoc,
 setDoc
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-const container = document.getElementById("pageContainer");
+const container =
+document.getElementById(
+"pageContainer"
+);
 
-onAuthStateChanged(auth, async(user)=>{
+onAuthStateChanged(
+auth,
+async(user)=>{
 
 if(!user){
-location.href = "login.html";
-return;
+
+  location.href =
+  "login.html";
+
+  return;
 }
 
-const userRef = doc(db,"users",user.uid);
-const userSnap = await getDoc(userRef);
+const userRef =
+doc(
+  db,
+  "users",
+  user.uid
+);
+
+const userSnap =
+await getDoc(
+  userRef
+);
 
 if(!userSnap.exists()){
-await setDoc(userRef,{
-balance:100
-});
+
+  await setDoc(
+    userRef,
+    {
+      balance:100
+    }
+  );
+
 }
 
-loadPage("dashboard");
+await loadPage(
+  "dashboard"
+);
 
-const admin = await isAdmin(user.uid);
+const admin =
+await isAdmin(
+  user.uid
+);
 
 const adminBtn =
 document.querySelector(
-'[data-page="admin"]'
+  '[data-page="admin"]'
 );
 
 if(adminBtn){
-adminBtn.style.display =
-admin ? "block" : "none";
+
+  adminBtn.style.display =
+  admin
+  ? "block"
+  : "none";
+
 }
 
-});
+}
+);
 
 async function loadPage(page){
 
+try{
+
 const res =
-await fetch("pages/${page}.html");
+await fetch(
+  `pages/${page}.html`
+);
 
 const html =
 await res.text();
 
-container.innerHTML = html;
+container.innerHTML =
+html;
 
 const scripts =
-container.querySelectorAll("script");
-
-scripts.forEach(oldScript=>{
-
-const newScript =
-document.createElement("script");
-
-newScript.type =
-oldScript.type || "module";
-
-newScript.textContent =
-oldScript.textContent;
-
-oldScript.parentNode.replaceChild(
-  newScript,
-  oldScript
+container.querySelectorAll(
+  "script"
 );
 
-});
+scripts.forEach(
+  oldScript=>{
+
+    const newScript =
+    document.createElement(
+      "script"
+    );
+
+    newScript.type =
+    oldScript.type ||
+    "module";
+
+    newScript.textContent =
+    oldScript.textContent;
+
+    oldScript.parentNode
+    .replaceChild(
+      newScript,
+      oldScript
+    );
+
+  }
+);
 
 }
 
-document.addEventListener("click",(e)=>{
+catch(err){
+
+console.error(err);
+
+container.innerHTML = `
+  <div class="card">
+    Failed To Load Page
+  </div>
+`;
+
+}
+
+}
+
+document.addEventListener(
+"click",
+(e)=>{
 
 const btn =
-e.target.closest("[data-page]");
+e.target.closest(
+  "[data-page]"
+);
 
 if(!btn) return;
 
-loadPage(btn.dataset.page);
+loadPage(
+  btn.dataset.page
+);
 
-});
+}
+);
 
-window.logout = async()=>{
+window.logout =
+async()=>{
 
-await signOut(auth);
+await signOut(
+auth
+);
 
-location.href = "login.html";
+location.href =
+"login.html";
 
 };
