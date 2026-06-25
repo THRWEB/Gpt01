7a0e9539544ffffb30250c831523071042b0cae7
thr <t31725@gmail.com>
2026-06-25 11:45:36 +0530

    Update app-ui.js

--- js/app-ui.js
+++ js/app-ui.js
@@ -1,58 +1,98 @@
-import { auth, isAdmin } from "./firebase.js";
+import { auth, isAdmin, db } from "./firebase.js";
 
 import {
-  onAuthStateChanged,
-  signOut
+onAuthStateChanged,
+signOut
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+doc,
+getDoc,
+setDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const container = document.getElementById("pageContainer");
 
 onAuthStateChanged(auth, async(user)=>{
 
-  if(!user){
-    location.href = "login.html";
-    return;
-  }
+if(!user){
+location.href = "login.html";
+return;
+}
 
-  loadPage("dashboard");
+const userRef = doc(db,"users",user.uid);
+const userSnap = await getDoc(userRef);
 
-  // ✅ ADMIN BUTTON CONTROL
-  const admin = await isAdmin(user.uid);
+if(!userSnap.exists()){
+await setDoc(userRef,{
+balance:100
+});
+}
 
-  const adminBtn = document.querySelector('[data-page="admin"]');
+loadPage("dashboard");
 
-  if(adminBtn){
-    adminBtn.style.display = admin ? "block" : "none";
-  }
+const admin = await isAdmin(user.uid);
+
+const adminBtn =
+document.querySelector(
+'[data-page="admin"]'
+);
+
+if(adminBtn){
+adminBtn.style.display =
+admin ? "block" : "none";
+}
 
 });
 
 async function loadPage(page){
 
-  const res = await fetch(`pages/${page}.html`);
-  const html = await res.text();
+const res =
+await fetch("pages/${page}.html");
 
-  container.innerHTML = html;
+const html =
+await res.text();
 
-  const scripts = container.querySelectorAll("script");
+container.innerHTML = html;
 
-  scripts.forEach(oldScript=>{
-    const newScript = document.createElement("script");
-    newScript.type = oldScript.type || "module";
-    newScript.textContent = oldScript.textContent;
-    oldScript.parentNode.replaceChild(newScript, oldScript);
-  });
+const scripts =
+container.querySelectorAll("script");
+
+scripts.forEach(oldScript=>{
+
+const newScript =
+document.createElement("script");
+
+newScript.type =
+oldScript.type || "module";
+
+newScript.textContent =
+oldScript.textContent;
+
+oldScript.parentNode.replaceChild(
+  newScript,
+  oldScript
+);
+
+});
+
 }
 
 document.addEventListener("click",(e)=>{
 
-  const btn = e.target.closest("[data-page]");
-  if(!btn) return;
+const btn =
+e.target.closest("[data-page]");
 
-  loadPage(btn.dataset.page);
+if(!btn) return;
+
+loadPage(btn.dataset.page);
+
 });
 
 window.logout = async()=>{
-  await signOut(auth);
-  location.href = "login.html";
+
+await signOut(auth);
+
+location.href = "login.html";
+
 };
